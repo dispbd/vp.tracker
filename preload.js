c8f1a3200142fe7a9186e19f6191e7ef80c101a3
Vivtonenko Alexandr <dispbd@gmail.com>
2020-12-31 02:06:45 +0300

    big upd

--- preload.js
+++ preload.js
@@ -1,40 +1,40 @@
 const {
-  contextBridge,
-  ipcRenderer
+    contextBridge,
+    ipcRenderer
 } = require("electron");
 
-const validChannels = ["toMain", "fromMain"];
+const validChannels = ["to", "from"];
 
 // Expose protected methods that allow the renderer process to use the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld(
-  "api", {
-    send: (channel, data) => {
-      //console.log(channel, data);
-      if (validChannels.includes(channel)) { // whitelist channels
-        ipcRenderer.send(channel, data);
-      }
-    },
-    receive: (channel, func) => {
-      //console.log(channel);
-      if (validChannels.includes(channel)) {
-        ipcRenderer.on(channel, (event, ...args) => func(...args)); // Deliberately strip event as it includes `sender` 
-      }
-    },
-    // once: (channel, callback) => {
-    //   if (validChannels.includes(channel)) {
-    //     const newCallback = (_, data) => callback(data);
-    //     ipcRenderer.once(channel, newCallback);
-    //   }
-    // },
-    // removeListener: (channel, callback) => {
-    //   if (validChannels.includes(channel)) {
-    //     ipcRenderer.removeListener(channel, callback);
-    //   }
-    // },
-    // removeAllListeners: (channel) => {
-    //   if (validChannels.includes(channel)) {
-    //     ipcRenderer.removeAllListeners(channel)
-    //   }
-    // },
-  }
+    "api", {
+        send: (channel, data) => {
+            //console.log(channel, data);
+            if (validChannels.includes(channel)) { // whitelist channels
+                ipcRenderer.send(channel, data);
+            }
+        },
+        receive: (channel, func) => {
+            //console.log(channel);
+            if (validChannels.includes(channel)) {
+                ipcRenderer.on(channel, (event, ...args) => func(...args)); // Deliberately strip event as it includes `sender` 
+            }
+        },
+        // once: (channel, callback) => {
+        //   if (validChannels.includes(channel)) {
+        //     const newCallback = (_, data) => callback(data);
+        //     ipcRenderer.once(channel, newCallback);
+        //   }
+        // },
+        // removeListener: (channel, callback) => {
+        //   if (validChannels.includes(channel)) {
+        //     ipcRenderer.removeListener(channel, callback);
+        //   }
+        // },
+        // removeAllListeners: (channel) => {
+        //   if (validChannels.includes(channel)) {
+        //     ipcRenderer.removeAllListeners(channel)
+        //   }
+        // },
+    }
 );
